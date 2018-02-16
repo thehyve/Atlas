697e577d42784f73cea3e3e089469d42074d8740
Maxim Moinat <maxim@thehyve.nl>
2018-02-16 15:22:19 +0100

    by default time at risk ends with 365 days

--- js/modules/iranalysis/inputTypes/FieldOffset.js
+++ js/modules/iranalysis/inputTypes/FieldOffset.js
@@ -1,11 +1,11 @@
 define(['knockout'], function (ko) {
 
-	function FieldOffset(data) {
+	function FieldOffset(data, defaultOffset = 0) {
 		var self = this;
 		data = data || {};
 
 		self.DateField = ko.observable(data.DateField || "StartDate");
-		self.Offset = ko.observable(data.Offset || 0);
+		self.Offset = ko.observable(data.Offset || defaultOffset);
 	}
 
 	FieldOffset.prototype.toJSON = function () {
--- js/modules/iranalysis/inputTypes/TimeAtRisk.js
+++ js/modules/iranalysis/inputTypes/TimeAtRisk.js
@@ -3,9 +3,10 @@ define(['knockout', './FieldOffset'], function (ko, FieldOffset) {
 	function TimeAtRisk(data) {
 		var self = this;
 		data = data || {};
-		
-		self.start = new FieldOffset(data.start);
-		self.end = new FieldOffset(data.end);
+
+		// Default from 0 to 365 days from start
+		self.start = new FieldOffset(data.start, 0);
+		self.end = new FieldOffset(data.end, 365);
 
 	}
 	
