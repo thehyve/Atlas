bae3b4130015ebd71919b33dd65933de88b5b2b6
Maxim Moinat <maxim@thehyve.nl>
2018-03-01 16:44:38 +0100

    moved all logic from html to javascript

--- js/modules/iranalysis/components/dashboard.js
+++ js/modules/iranalysis/components/dashboard.js
@@ -15,12 +15,54 @@ define(['knockout',
         self.dirtyFlag = params.dirtyFlag;
         self.analysisCohorts = params.analysisCohorts;
 
+        self.mode = ko.observable("case");
+        self.rateMultiplier = ko.observable(1000);
+        self.results = ko.observableArray();
+
         self.getResults = function (source, targetId, outcomeId) {
             var summaryList = source.info().summaryList;
             return summaryList.filter(function (item) {
                 return (item.targetId == targetId && item.outcomeId == outcomeId);
             })[0] || {totalPersons: 0, cases: 0, timeAtRisk: 0};
         };
+
+        self.loadResults = function(source) {
+            var summaryList = source.info().summaryList;
+            self.analysisCohorts().targetCohorts().forEach(function(targetCohort) {
+                var row = ko.observableArray();
+                self.analysisCohorts().outcomeCohorts().forEach(function(outcomeCohort) {
+                    var info = summaryList.filter(function (item) {
+                        return (item.targetId == targetCohort.id && item.outcomeId == outcomeCohort.id);
+                    })[0] || {totalPersons: 0, cases: 0, timeAtRisk: 0};
+
+                    var column = ko.computed(function() {
+                        switch(self.mode()) {
+                            case "case":
+                                return info.cases;
+                            case "proportion":
+                                if (info.totalPersons > 0)
+                                    return (info.cases / info.totalPersons * self.rateMultiplier()).toFixed(2);
+                                else
+                                    return "NA";
+                            case "rate":
+                                if (info.timeAtRisk > 0)
+                                    return (info.cases / (info.timeAtRisk)* self.rateMultiplier()).toFixed(2);
+                                else
+                                    return "NA";
+                            default:
+                                return "-";
+                        }
+                    });
+
+                   row.push({'column':column});
+                });
+                self.results.push({'name':targetCohort.name, 'data':row});
+            })
+        };
+
+        if (self.sources().length > 0) {
+            self.loadResults(self.sources()[0]);
+        }
 
     }
 
