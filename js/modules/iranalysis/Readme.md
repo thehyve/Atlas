7615e8c495f734a345d40a88a2f3f494833497e2
Maxim Moinat <maxim@thehyve.nl>
2018-03-20 13:36:03 +0100

    strata table mockup

--- js/modules/iranalysis/Readme.md
+++ js/modules/iranalysis/Readme.md
@@ -21,7 +21,7 @@ In results.js/dashboard.js:
       * `sourceDialect`
       * `daimons`[]
     * `info`
-    Retrieved from WebApi: [http://localhost:8080/WebAPI/ir/3/info]()
+    Retrieved from WebApi: [http://localhost:8080/WebAPI/ir/15/info]()
       * `summaryList`[] For every target/outcome pair
         * `targetId`
         * `outcomeId`
@@ -30,4 +30,38 @@ In results.js/dashboard.js:
         * `cases`
       * `executionInfo`
         * `id.analysisId`, `id.sourceId`, `startTime`, `executionDuration`, `status`, `isValid`, `message`
-    
+- Stratification `report` per target & outcome (and data source)
+ As returned from `iraApi.getReport(irId, sourceKey, targetId, outcomeId)`
+ [http://localhost:8080/WebAPI/ir/15/report/SYN?targetId=5&outcomeId=2]()
+  * `summary`
+    * `targetId`
+    * `outcomeId`
+    * `totalPersons`
+    * `timeAtRisk`
+    * `cases`
+  * `stratifyStats`[] For every stratify rule
+    * `targetId`
+    * `outcomeId`
+    * `id` stratify id (starts at 0)
+    * `name` stratfiy name
+    * `totalPersons`
+    * `timeAtRisk`
+    * `cases`
+- PROPOSED summary `report` per data source
+ [http://localhost:8080/WebAPI/ir/15/reports/SYN]()
+  * []
+    * `summary`
+        * `targetId`
+        * `outcomeId`
+        * `totalPersons`
+        * `timeAtRisk`
+        * `cases`
+    * `stratifyStats`[]
+        * `targetId`
+        * `outcomeId`
+        * `id` stratify id (starts at 0)
+        * `name` stratify name
+        * `totalPersons`
+        * `timeAtRisk`
+        * `cases`
+    * `treemapData`
--- js/modules/iranalysis/components/dashboard.js
+++ js/modules/iranalysis/components/dashboard.js
@@ -13,44 +13,194 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-define(['knockout', 'text!./dashboard.html', 'databindings'], function (ko, template) {
+define(['knockout', 'text!./dashboard.html', 'webapi/IRAnalysisAPI', 'databindings'], function (ko, template, iraApi) {
     function dashboardResultsViewer(params) {
         var self = this;
 
         self.sources = params.sources;
         // self.sources().push({'source':{'sourceName':'Mock Source', 'sourceKey':'mock'},'info':ko.observable({'summaryList':[]})}); // Sometimes overridden, as self.sources can be refreshed from other component
         self.dirtyFlag = params.dirtyFlag;
-        self.analysisCohorts = params.analysisCohorts;
+        self.targetCohorts = params.analysisCohorts().targetCohorts;
+        self.outcomeCohorts = params.analysisCohorts().outcomeCohorts;
+        self.strata = params.strata;
         self.selectedSourceKey = ko.observable();
         self.cohortMatrix = ko.observableArray();
         self.availableModes = ko.observableArray(["Cases","Proportion","Rate"]);
         self.activeMode = ko.observable("Cases");
         self.rateSteps = ko.observable(3);
 
-        self.sources.subscribe(function() {
-            if (self.sources().length > 0)
-                self.loadCohortMatrix()
-        });
+        var Target = function(id, name) {
+            var self = this;
+            self.id = id;
+            self.targetName = name;
+            self.reports = ko.observableArray();
+            self.strata = ko.observableArray();
+            self.doShowStrata = ko.observable(false);
 
-        self.loadCohortMatrix = function() {
-            // For each target and outcome pair, add a cell containing the cases, proportion or rate.
-            self.cohortMatrix = ko.observableArray();
-            self.analysisCohorts().targetCohorts().forEach(function(targetCohort) {
-                var outcomeCells = ko.observableArray();
-                self.analysisCohorts().outcomeCohorts().forEach(function(outcomeCohort) {
-                    var cell = ko.computed(function() {
-                        var irData = self.getIRdata(self.selectedSourceKey(), targetCohort.id, outcomeCohort.id);
-                        return self.cellValue(irData);
-                    });
-                    outcomeCells.push(cell);
-                });
+            self.targetNameHtml = ko.pureComputed(function() {
+                if (self.strata().length === 0) {
+                    return self.targetName;
+                }
+                if (self.doShowStrata()) {
+                    return "<i class=\"fa fa-minus-square\"></i> " + self.targetName;
+                }
+                return "<i class=\"fa fa-plus-square\"></i> " + self.targetName;
+            });
 
-                self.cohortMatrix.push({
-                    'targetCohortName':targetCohort.name,
-                    'outcomeCells':outcomeCells
-                });
+            self.toggleStrata = function() {
+                self.doShowStrata(!self.doShowStrata());
+            };
+
+            self.addReport = function(report) {
+                this.reports.push(report);
+            };
+
+            self.addStratum = function(stratum) {
+                this.strata.push(stratum);
+            };
+
+            self.getReport = function(index) {
+                return this.reports()[index];
+            };
+
+            self.getStratum = function(index) {
+                return this.strata()[index];
+            };
+        };
+
+        var Stratum = function(id, name) {
+            var self = this;
+            self.id = id;
+            self.stratumName = name;
+            self.reports = ko.observableArray();
+
+            self.add = function(report) {
+                this.reports.push(report);
+            };
+
+            self.getReport = function(index) {
+                return this.reports()[index];
+            };
+        };
+
+        var Report = function(master, id) {
+            var self = this;
+            self.master = master;
+            self.outcomeId = id;
+            // TODO: fetch real numbers
+            // Steps: 1. make this object aware of its target, outcome and stratum id. 2. create an observable object in the master 3. update this data object whenever data becomes available.
+            self.cases = (Math.random()*10).toFixed(0);
+            self.totalPersons = self.cases * 3;
+            self.timeAtRisk = self.cases * 6;
+
+            self.value = ko.pureComputed(function() {
+                switch(self.master.activeMode()) {
+                    case "Cases":
+                        return self.cases;
+                    case "Proportion":
+                        if (self.totalPersons > 0)
+                            return (self.cases / self.totalPersons * self.master.rateMultiplier()).toFixed(2);
+                        else
+                            return "NA";
+                    case "Rate":
+                        if (self.timeAtRisk > 0)
+                            return (self.cases / self.timeAtRisk * self.master.rateMultiplier()).toFixed(2);
+                        else
+                            return "NA";
+                    default:
+                        return "-";
+                }
             });
         };
+
+        self.init = function() {
+            // Create table with the given number of targets, outcomes and strata.
+            self.targets = ko.observableArray();
+            self.targetCohorts().forEach(function(targetCohort) {
+                var target = new Target(targetCohort.id, targetCohort.name);
+
+                // Create a report for each outcome
+                self.outcomeCohorts().forEach(function(outcomeCohort) {
+                    target.addReport(new Report(self, outcomeCohort.id));
+                });
+
+                // Create a report for each stratum and outcome
+                for(var i = 0;i<self.strata().length;i++) {
+                    var stratum = new Stratum(i, self.strata()[i].name);
+                    self.outcomeCohorts().forEach(function(outcomeCohort) {
+                        stratum.add(new Report(self, outcomeCohort.id));
+                    });
+                    target.addStratum(stratum);
+                }
+
+                self.targets.push(target);
+            });
+        };
+
+        // self.sources.subscribe(function() {
+        //     if (self.sources().length > 0)
+        //         self.loadCohortMatrix()
+        // });
+        //
+        // self.loadCohortMatrix = function() {
+        //     // For each target and outcome pair, add a cell containing the cases, proportion or rate.
+        //     self.cohortMatrix = ko.observableArray();
+        //     self.analysisCohorts().targetCohorts().forEach(function(targetCohort) {
+        //         var summaryRow = ko.observableArray();
+        //         self.analysisCohorts().outcomeCohorts().forEach(function(outcomeCohort) {
+        //             var cell = ko.computed(function() {
+        //                 var irData = self.getIRdata(self.selectedSourceKey(), targetCohort.id, outcomeCohort.id);
+        //                 return self.cellValue(irData);
+        //             });
+        //             summaryRow.push(cell);
+        //         });
+        //
+        //         self.cohortMatrix.push({
+        //             'targetCohortName': targetCohort.name,
+        //             'stratumName': null,
+        //             'outcomeCells': summaryRow
+        //         });
+        //
+        //         for(var stratumId = 0;stratumId<self.strata().length;stratumId++) {
+        //             var stratum = self.strata()[stratumId];
+        //             var stratumRow = ko.observableArray();
+        //             self.analysisCohorts().outcomeCohorts().forEach(function(outcomeCohort) {
+        //                 var strataObject = self.strataObjects[targetCohort.id][outcomeCohort.id][stratumId];
+        //
+        //                 iraApi.getReport(
+        //                     self.sources()[0].info().executionInfo.id.analysisId,
+        //                     "CDM5", //self.selectedSourceKey()
+        //                     targetCohort.id,
+        //                     outcomeCohort.id
+        //                 ).then(function(report) {
+        //                     var stratumStat = report.stratifyStats.find(function(stat){return stat.id === stratumId});
+        //                     strataObject(stratumStat);
+        //                 });
+        //
+        //                 var cell = ko.computed(function() {
+        //                     if (stratumId >= self.strata().length) {
+        //                         console.log("Huh, this is strange");
+        //                         return "";
+        //                     }
+        //                     // console.log(targetCohort.id);
+        //                     // console.log(outcomeCohort.id);
+        //                     // console.log(stratumId);
+        //                     // console.log(self.strataObjects);
+        //                     return self.cellValue(strataObject());
+        //                 });
+        //
+        //                 stratumRow.push(cell);
+        //             });
+        //
+        //             self.cohortMatrix.push({
+        //                 'targetCohortName': null,
+        //                 'stratumName': stratum.name(),
+        //                 'outcomeCells': stratumRow
+        //             });
+        //         }
+        //
+        //     });
+        // };
 
         // Table may only be produced when sources are available
         self.sourcesAvailable = ko.pureComputed(function() {
@@ -78,40 +228,23 @@ define(['knockout', 'text!./dashboard.html', 'databindings'], function (ko, temp
             }
         });
 
-        self.cellValue = function(irData) {
-            switch(self.activeMode()) {
-                case "Cases":
-                    return irData.cases;
-                case "Proportion":
-                    if (irData.totalPersons > 0)
-                        return (irData.cases / irData.totalPersons * self.rateMultiplier()).toFixed(2);
-                    else
-                        return "NA";
-                case "Rate":
-                    if (irData.timeAtRisk > 0)
-                        return (irData.cases / irData.timeAtRisk * self.rateMultiplier()).toFixed(2);
-                    else
-                        return "NA";
-                default:
-                    return "-";
-            }
-        };
+        // self.getIRdata = function (sourceKey, targetId, outcomeId) {
+        //     var source = self.sources().filter(function (item) {
+        //         return (item.source.sourceKey === sourceKey);
+        //     })[0];
+        //     if (!source) {
+        //         return {totalPersons: 0, cases: 0, timeAtRisk: 0};
+        //     }
+        //
+        //     return source.info().summaryList.filter(function (item) {
+        //         return (item.targetId === targetId && item.outcomeId === outcomeId);
+        //     })[0] || {totalPersons: 0, cases: 0, timeAtRisk: 0};
+        // };
 
-        self.getIRdata = function (sourceKey, targetId, outcomeId) {
-            var source = self.sources().filter(function (item) {
-                return (item.source.sourceKey === sourceKey);
-            })[0];
-            if (!source) {
-                return {totalPersons: 0, cases: 0, timeAtRisk: 0};
-            }
+        // if (self.sources().length > 0)
+        //     self.loadCohortMatrix();
 
-            return source.info().summaryList.filter(function (item) {
-                return (item.targetId === targetId && item.outcomeId === outcomeId);
-            })[0] || {totalPersons: 0, cases: 0, timeAtRisk: 0};
-        };
-
-        if (self.sources().length > 0)
-            self.loadCohortMatrix();
+        self.init();
     }
 
     var component = {
